d49107bf918af46cc26c12774f70438b906eab54
Kipngeno Abednego <abnasgit@gmail.com>
2025-06-28 22:52:11 +0300

    remove commented out code

--- app/utils/decorators.py
+++ app/utils/decorators.py
@@ -1,19 +1,6 @@
 from functools import wraps
 from flask import flash, redirect, url_for
 from flask_login import current_user
-
-# def admin_required(f):
-#     """Decorator to restrict access to admin users"""
-#     @wraps(f)
-#     def decorated_function(*args, **kwargs):
-#         if not current_user.is_admin():
-#             flash('Admin access required.', 'error')
-#             return redirect(url_for('main.index'))
-#         return f(*args, **kwargs)
-#     return decorated_function
-
-# Create a custom decorator in auth/utils.py
-
 from flask import abort, current_app
 
 
