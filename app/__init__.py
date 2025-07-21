4cd0c1c6f5610ed3158dec12532b4f3e1e5776b7
abnasity <abnasgit@gmail.com>
2025-07-21 18:30:37 +0300

    cloudinary config

--- app/__init__.py
+++ app/__init__.py
@@ -7,11 +7,15 @@ from app.extensions import db, migrate, login_manager, bcrypt, csrf, mail
 from flask_wtf.csrf import generate_csrf
 from flask_login import current_user
 from app.models import CartItem, User, Customer, CustomerOrder, Notification
-
 from app.utils.cloudinary_utils import init_cloudinary
+import cloudinary
 
 
-
+cloudinary.config(
+    cloud_name=os.getenv('CLOUDINARY_CLOUD_NAME'),
+    api_key=os.getenv('CLOUDINARY_API_KEY'),
+    api_secret=os.getenv('CLOUDINARY_API_SECRET')
+)
 
 @login_manager.unauthorized_handler
 def custom_unauthorized():
