09e6b94e416786279e5c491ec3e13a1937f20b12
abnas <abnasgit@gmail.com>
2025-07-25 22:20:06 +0300

    modify wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,18 +1,14 @@
 import os
 from app import create_app
-from app.extensions import db
 
-# ✅ Only load .env locally (not in production)
+# Only load .env locally
 if os.environ.get("FLASK_ENV") != "production":
     from dotenv import load_dotenv
     load_dotenv()
 
-# Create app
 app = create_app()
 
 if __name__ == "__main__":
     debug = os.environ.get("FLASK_DEBUG", "1") == "1"
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=debug, port=port)
-
-app = create_app()
+    app.run(debug=debug, host='0.0.0.0', port=port)
