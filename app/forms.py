605f622a3bab71fec0ecb7435b4099819d957409
Kipngeno Abednego <abnasgit@gmail.com>
2025-10-11 13:54:46 +0300

    remove credit price

--- app/forms.py
+++ app/forms.py
@@ -100,7 +100,6 @@ class DeviceForm(FlaskForm):
     )
     purchase_price = DecimalField("Purchase Price", validators=[DataRequired(), NumberRange(min=0)], places=2)
     price_cash = DecimalField("Selling Price", validators=[Optional(), NumberRange(min=0)])
-    price_credit = DecimalField("Credit Price", validators=[Optional(), NumberRange(min=0)])
     description = TextAreaField("Description", validators=[Optional()])
     assigned_staff_id = SelectField("Assign to Staff", coerce=int, validators=[DataRequired()])
     submit = SubmitField("Save Device")
